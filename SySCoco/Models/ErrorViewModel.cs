477dc4482d9cd03caba3e69ee03d18ee65546e98
Sebastianquintero <59904141+Sebastianquintero@users.noreply.github.com>
2025-02-18 15:30:05 -0500

    Delete SySCoco directory

--- SySCoco/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SySCoco.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
